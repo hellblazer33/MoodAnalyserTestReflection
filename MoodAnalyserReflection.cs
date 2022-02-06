33e58312d5aa070f52447ec7cbd5f460ec49e53c
Pankaj Kumar <pk110495@gmail.com>
2022-02-06 17:10:32 +0530

    added mood analyser reflection case

---
+++ MoodAnalyserReflection.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzer;
+
+namespace MoodAnalyserTest
+{
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject()
+        {
+            string message = null;
+            object expected = new MoodAnalyser(message);
+            object obj = MoodAnalyserFactory.CreateMoodAnalyser("MoodAnalyserAppWithCore.MoodAnalyser", "MoodAnalyser");
+            expected.Equals(obj);
+            Assert.AreNotEqual(expected, obj);
+
+        }
+
+      }
+}
